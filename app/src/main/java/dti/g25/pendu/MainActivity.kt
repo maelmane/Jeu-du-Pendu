871bc632df2b2a0f3dfe95a0e5aa644be72a7de7
Patrick Lafrance <patrick.lafrance@gmail.com>
2023-03-19 18:28:50 -0500

    Début de projet avec interface

---
+++ app/src/main/java/dti/g25/pendu/MainActivity.kt
@@ -0,0 +1,17 @@
+package dti.g25.pendu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
